33b5a565982150c381b2fb7b9aa1d7b9b461e60a
irisbune <irisbune@gmail.com>
2016-02-09 14:37:42 +0100

    added offers functionality

---
+++ app/controllers/offerings_controller.rb
@@ -0,0 +1,29 @@
+class OfferingsController < ApplicationController
+
+  def index
+    @offers = Offering.all
+  end
+
+  def new
+    @offer = Offering.new
+    @boats = Boat.all
+  end
+
+  def create
+    @period_start
+    @period_end
+    if @offer = Offering.create(offer_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def offer_params
+    params.require(:offering).permit(:price, :period_start, :period_end, :location, :boat_id)
+  end
+
+end
