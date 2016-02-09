8a421120f23e4b55d253339bc3b0273530d6a079
Maurice <mcsmorenberg@gmail.com>
2016-02-09 20:09:55 +0100

    modified reservation controller and view added

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,31 @@
 class ReservationsController < ApplicationController
+
+  def index
+    @reservations = Reservation.all
+  end
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.create(params)
+  end
+
+  def show
+    @reservation = Reservation.find(params)
+  end
+
+  def edit
+    @reservation = Reservation.find(params)
+  end
+
+  def update
+    @reservation = Reservation.find(params)
+  end
+
+  def destroy
+    @reservation = Reservation.find(params)
+  end
+  
 end
