15380a6994dda818570ed1429c3c273c65590a7b
Chiel Hackman <chielhackman@gmail.com>
2016-02-11 16:44:56 +0100

    Fixed

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,11 +1,4 @@
 class Boat < ActiveRecord::Base
-<<<<<<< HEAD
   has_many :offers, dependent: :destroy
   belongs_to :users
-||||||| merged common ancestors
-  has_many :offers
-=======
-  has_many :offers
-  belongs_to :users
->>>>>>> d5ef32b0b52608a671b5b24a853a137b8748fa79
 end
