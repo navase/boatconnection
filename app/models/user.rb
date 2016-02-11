bba18a068edbba3013b7fdfe15d59493da31d83c
irisbune <irisbune@gmail.com>
2016-02-11 19:21:49 +0100

    integrating devise. signup works. updating profile works

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_many :boats, dependent: :destroy
   has_many :reservations, dependent: :destroy
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
